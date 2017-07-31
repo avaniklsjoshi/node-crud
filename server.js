4b08a52548c0653e2e3c876d90929d46f145752d
Avani joshi <avaniklsjoshi@gmail.com>
2017-07-31 18:26:12 +0530

    env variable

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+//load env variables
+require('dotenv').config();
+
 // grab the dependencies
-const express= require('express'),
-      app=express(),
-      port=process.env.PORT || 7000,
-      expressLayouts=require('express-ejs-layouts');
+const express        = require('express'),
+      app            = express(),
+      port           = process.env.PORT || 7000,
+      expressLayouts = require('express-ejs-layouts'),
+      mongoose       = require('mongoose');
  
 // configure our app
 // tell express where to look for static assets
@@ -11,6 +15,9 @@ app.use(express.static(__dirname+'/public'));
 // set ejs as our templating engine
 app.set('view engine','ejs');
 app.use(expressLayouts);
+
+// connect to db
+mongoose.connect(process.env.DB_URI);
 
 // set the routes
 app.use(require('./app/routes'));
