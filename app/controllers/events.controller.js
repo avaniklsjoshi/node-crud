4eb4e95eac2b498a86913d26c6ef13b93d77e1ef
Avani joshi <avaniklsjoshi@gmail.com>
2017-08-01 11:19:29 +0530

    database seeded

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,20 +1,51 @@
+const Event=require('../models/event');
+
 module.exports={
-
-  // show all events
-  showEvents:(req,res)=>{
-    //dummy events
-    const events=[
-      {name:'Basketball', slug:'basketball',description:'Throwing into a basket.'},
-      {name:'Swimming', slug:'swimming',description:'Avani is the fast fish.'},
-      {name:'WeightLifting', slug:'weightLifting',description:'Lifting heavy things up.'},
-      {name:'Basketball', slug:'basketball',description:'Throwing into a basket.'},
-    ];
-    res.render('pages/events',{events:events});
-  },
-
-  showSingle:(req, res)=>{
-    const event= {name:'Basketball', slug:'basketball',description:'Throwing into a basket.'};
-    res.render('pages/single',{event:event});
-  }
-
+  showEvents:showEvents,
+  showSingle:showSingle,
+  seedEvents:seedEvents
 };
+
+/**
+ * show single event
+ */
+function showSingle(req, res){
+  const event= {name:'Basketball', slug:'basketball',description:'Throwing into a basket.'};
+  res.render('pages/single',{event:event});
+}
+
+/**
+ * show all events
+ */
+function showEvents(req,res){
+     
+    // get all events
+    
+    // return a view with data
+    res.render('pages/events',{events:events});
+}
+/**
+ * seed our db
+ */
+function seedEvents(req,res){
+  // create some events
+  const events        = [
+    {name:              'Basketball',description:    'Throwing into a basket.'},
+    {name:              'Swimming', description:     'Avani is the fast fish.'},
+    {name:              'WeightLifting',description: 'Lifting heavy things up.'},
+    {name:              'Ping Pong',description:     'super fast ball.'}
+
+  ];
+
+  // use the event model to insert/save, but firt empty it.
+  Event.remove({},() => {
+    // use the event model to insert/save
+    for(event of events){
+      var newEvent    = new Event(event);
+      newEvent.save();
+    }
+  }); 
+
+  //seeded!
+  res.send('db seeded!')
+}
--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,18 @@ const express=require('express');
 
 module.exports=router;
 
+// main routes
 router.get('/', mainController.showHome);
 
+// event routes
 router.get('/events', eventsController.showEvents);
+
+// seed events
+router.get('/events/seed', eventsController.seedEvents);
+
+// create events
+// edit events
+// delete events
+
+// show single event
 router.get('/events/:slug',eventsController.showSingle);
